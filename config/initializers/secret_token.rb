50e81fe7f929ac74607cf5e7fcc35033c8880060
swapnil <swapnil.patil04@gmail.com>
2012-10-27 19:10:25 +0530

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogtwitter::Application.config.secret_token = '9af23670a8f97668396f1679ebfe4551248f4810aed2121dc7f0de7a8bf9e1d05186048c1cd1b6830c838ef8a4573887d92bd72d47abba4f40fe19a5bb54ba61'
